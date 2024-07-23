24e052cb1f5bfbf0f7e891efe4a6252fd769358c
EMMANUEL LUCKY <emmanueltech2019@gmail.com>
2024-07-24 00:24:43 +0100

    update

--- src/app/_document.tsx
+++ src/app/_document.tsx
@@ -1,6 +1,6 @@
 // app/_document.js
 
-import Document, { Html, Head, Main, NextScript } from 'next/document';
+import  { Html, Head, Main, NextScript } from 'next/document';
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
